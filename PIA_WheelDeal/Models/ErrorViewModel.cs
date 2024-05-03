69fcbdb3e1ba97631e45b06954e7a8bb6262366f
KevinKastro25 <kevin@DESKTOP-3VI4AUG>
2024-05-03 14:20:46 -0600

    Agregar archivos de proyecto.

---
+++ PIA_WheelDeal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PIA_WheelDeal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
